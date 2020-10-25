408d1307df7f5c941f7cbf20bc291eb3d8ec5d29
Twometer <twometer@outlook.de>
2020-10-25 23:31:12 +0100

    Update ingame UI

--- src/main/java/de/twometer/amongus/AmongUs.java
+++ src/main/java/de/twometer/amongus/AmongUs.java
@@ -40,12 +40,12 @@ public class AmongUs extends NekoApp {
         getFxManager().getBloom().setActive(true);
 
         // Base map
-        /*var skeld = ModelLoader.loadModel("TheSkeld.obj");
+        var skeld = ModelLoader.loadModel("TheSkeld.obj");
         skeld.streamTree()
                 .filter(m -> m instanceof ModelPart && m.getName().contains("Luces"))
                 .forEach(m -> getScene().addLight(new LightSource(m.getCenter())));
         getScene().addModel(skeld);
-*/
+
 
         // Sky
         var skyboxCubemap = TextureLoader.loadCubemap("Sky/right.png", "Sky/left.png", "Sky/top.png", "Sky/bottom.png", "Sky/front.png", "Sky/back.png");
