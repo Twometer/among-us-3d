1a637ede4dfd26dcf0c42ae832e1ef3d15aece69
Twometer <twometer@outlook.de>
2021-10-06 17:42:01 +0200

    Fix bug in SML row

--- src/main/kotlin/de/twometer/amogus/res/SmlRow.kt
+++ src/main/kotlin/de/twometer/amogus/res/SmlRow.kt
@@ -1,12 +1,18 @@
 package de.twometer.amogus.res
 
+import org.joml.Vector2f
 import org.joml.Vector3f
 
 class SmlRow(private val data: List<String>) {
 
     private var index = 0
 
-    val hasRemaining get() = index < data.size - 1
+    val hasRemaining get() = index < data.size
+
+    fun readVec2(): Vector2f {
+        val parts = readString().split(",")
+        return Vector2f(parts[0].toFloat(), parts[1].toFloat())
+    }
 
     fun readVec3(): Vector3f {
         val parts = readString().split(",")
