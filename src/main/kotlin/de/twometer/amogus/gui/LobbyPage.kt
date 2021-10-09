d80aa08906be0f349de15d3548331450376d7d8f
Twometer <twometer@outlook.de>
2021-10-09 16:33:28 +0200

    Player updates in lobby

--- src/main/kotlin/de/twometer/amogus/gui/LobbyPage.kt
+++ src/main/kotlin/de/twometer/amogus/gui/LobbyPage.kt
@@ -1,7 +1,12 @@
 package de.twometer.amogus.gui
 
 import de.twometer.amogus.client.AmongUsClient
+import de.twometer.amogus.net.OnPlayerJoin
+import de.twometer.amogus.net.OnPlayerLeave
+import de.twometer.amogus.net.OnPlayerUpdate
 import de.twometer.amogus.net.StartGame
+import de.twometer.neko.events.Events
+import org.greenrobot.eventbus.Subscribe
 
 class LobbyPage : BasePage("Lobby.html") {
 
@@ -12,6 +17,22 @@ class LobbyPage : BasePage("Lobby.html") {
         AmongUsClient.session!!.players.forEach {
             call("addPlayer", it.id, it.username, it.color.name)
         }
+        Events.register(this)
+    }
+
+    override fun onUnloaded() {
+        Events.unregister(this)
+    }
+
+    @Subscribe
+    fun onPlayerLeave(e: OnPlayerLeave) {
+        call("removePlayer", e.id)
+    }
+
+    @Subscribe
+    fun onPlayerUpdate(e: OnPlayerUpdate) {
+        call("removePlayer", e.id)
+        call("addPlayer", e.id, e.username, e.color.name)
     }
 
     fun customize() {
